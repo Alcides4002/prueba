2826b53221b6669cbf8e9ef684ae04541579a846
Alcides4002 <carlos.mendoza13222@est.uca.edu.ni>
2022-10-11 21:48:32 -0600

    idea

---
+++ app/src/main/java/ui/game/fragmentos/fr1.kt
@@ -0,0 +1,11 @@
+package ui.game.fragmentos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fr1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fr1)
+    }
+}
